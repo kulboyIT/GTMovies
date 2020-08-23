ebaede10a8d1a941f6f7b18a5deffcbb26b1ff17
Patryck Gratão <patryck@polichat.com.br>
2020-08-23 04:56:57 -0300

    Refotaraçao

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,17 +7,17 @@ const movies = [
     "id": 1,
     "title": "O Protetor 2",
     "gender": "Ação",
-    "picture": "",
-    "embedURL": "https://www.youtube.com/watch?v=WgpqBkFFUVQ&",
-    "created_by": "Patryck Gratão",
+    "picture": "https://br.web.img3.acsta.net/pictures/18/06/19/13/51/0647704.jpg",
+    "movieCode": "WgpqBkFFUVQ",
+    "language": "dublado",
   },
   {
     "id": 2,
     "title": "Os vingadores",
     "gender": "Ação",
-    "picture": "",
-    "embedURL": "https://www.youtube.com/watch?v=WgpqBkFFUVQ&",
-    "created_by": "Patryck Gratão",
+    "picture": "https://br.web.img3.acsta.net/r_1280_720/medias/nmedia/18/89/43/82/20072617.jpg",
+    "movieCode": `4qRrM9IqZR0`,
+    "language": "dublado",
   }
 ];
 
@@ -28,15 +28,15 @@ routes.get('/movies',(req, res) => {
 
 // Create a new movie
 routes.post('/movies', (req, res) => {
-  const { id, title, picture, gender, embedURL, created_by } = req.body;
+  const { id, title, gender, picture, language, movieCode} = req.body;
 
   const newMovie = {
       id,
       title,
       gender,
       picture,
-      embedURL,
-      created_by,
+      language,
+      movieCode,
   }
   movies.push(newMovie);
   
@@ -46,7 +46,7 @@ routes.post('/movies', (req, res) => {
 // Update a movie
 routes.put('/movies/:id', (req, res) => {
   const { id } = req.params;
-  const { title, picture, gender, url, created_by } = req.body;
+  const { title, picture, language, gender, movieCode } = req.body;
   let currentMovie;
 
   movies.map((movie, index) => {
@@ -54,9 +54,9 @@ routes.put('/movies/:id', (req, res) => {
       currentMovie = Object.assign({}, movie, {})
       if (title) currentMovie.title = title;
       if (picture) currentMovie.picture = picture;
+      if (language) currentMovie.language = language;
       if (gender) currentMovie.gender = gender;
-      if (url) currentMovie.url = url;
-      if (created_by) currentMovie.created_by = created_by;
+      if (movieCode) currentMovie.movieCode = movieCode;
       
       movies[index] = currentMovie
     } else {
